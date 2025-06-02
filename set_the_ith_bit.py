8bab12a0bc9cea9957bdddc6978957a4421ac99b
Parthiv-06 <puthalapattuparthiv@gmail.com>
2025-06-02 15:28:22 +0530

    ith bit operations

--- set_the_ith_bit.py
+++ set_the_ith_bit.py
@@ -4,3 +4,21 @@ def set_the_ith_bit(val,i):
 val=9
 i=2
 print(set_the_ith_bit(val,i))
+
+# clear the ith set_the_ith_bit
+ 
+def clear_ith_bit(val,i):
+    x=val&(~(1<<i))
+    return x
+val=13
+i=2
+print(clear_ith_bit(val,i))
+
+#toggle the bit
+
+def toggele_ith_bit(val,i):
+    x=val^(1<<i)
+    return x
+val=13
+i=2
+print(toggele_ith_bit(val,i))
