f42a17f773b4568521343e72eb5b5e276aff1275
Parthiv-06 <puthalapattuparthiv@gmail.com>
2025-07-02 10:02:04 +0530

    ###

---
+++ stack-and-/next_greater_element_II.py
@@ -0,0 +1,15 @@
+def nextt_greater_element_circular(a):
+    res=[0]*len(a)
+    stack=[]
+    for i in range(2*len(a)-1,-1,-1):
+        while len(stack)!=0 and stack[-1]<a[i%len(a)]:
+            stack.pop()
+        if i<len(a):
+            if len(stack)==0:
+                res[i]=-1
+            elif stack[-1]>a[i]:
+                res[i]=stack[-1]
+        stack.append(a[i%len(a)])
+    return res
+a=[6,0,8,1,3]
+print(nextt_greater_element_circular(a))
