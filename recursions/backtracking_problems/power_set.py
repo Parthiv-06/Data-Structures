a1f130b8095de7bf07efbbb00c4336fddb18bcc9
Parthiv-06 <puthalapattuparthiv@gmail.com>
2025-08-06 10:17:05 +0530

    basic backtracking of recurssion

---
+++ recursions/backtracking_problems/power_set.py
@@ -0,0 +1,12 @@
+def power_set(n,i,arr,res):
+    if n==i:
+        print(res)
+        return
+    res.append(arr[i])
+    power_set(n,i+1,arr,res)
+    res.remove(arr[i])
+    power_set(n,i+1,arr,res)
+arr=[1,2,3,4,5]
+res=[]
+n=len(arr)
+power_set(n,0,arr,res)
